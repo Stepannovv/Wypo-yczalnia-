b4b4448da29e7d2bc60fd5829a0af0fa0c1bd900
Stepannovv <stepannovv@gmail.com>
2024-09-28 16:37:11 +0200

    footer

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import * as aboutme from './js/about-me.js';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+// AOS.init({
+//   duration: 4000,
+//   easing: 'ease-out-quart',
+// });
+AOS.init({
+  duration: 1000, // Adjusts animation speed
+  easing: 'ease-out-quart', // Easing style
+  once: false, // Ensures animation only happens once
+  anchorPlacement: 'top-bottom', // Start animation when element is in view
+});
